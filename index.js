1f143dfee3af43fe80dce4a5355c60ef35ead31e
Pardeep Singla <116626777+pardeep6443@users.noreply.github.com>
2024-07-13 13:11:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use("/api/comments", commentRoutes)
 
 app.use((err, req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    
     const status = err.status || 500;
     const message = err.status || "something went wrong!";
     return res.status(status).json({
