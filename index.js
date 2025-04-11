26f550ebe19b6cb9c404f901f0abcd1e38c5c093
Pardeep Singla <116626777+pardeep6443@users.noreply.github.com>
2025-04-11 22:20:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
-import express from "express"
-import cors from "cors"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-import userRoutes from "./routes/users.js"
-import videoRoutes from "./routes/videos.js"
-import commentRoutes from "./routes/comments.js"
-import authRoutes from "./routes/auth.js"
-import cookieParser from "cookie-parser"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/videos.js";
+import commentRoutes from "./routes/comments.js";
+import authRoutes from "./routes/auth.js";
+import cookieParser from "cookie-parser";
+import path from "path";
+
 const app = express();
 dotenv.config();
-
+const port = process.env.PORT || 8800;
 const connect = () => {
-    mongoose
-        .connect(process.env.MONGO)
-        .then(() => {
-            console.log("connected to DB!!")
-        })
-        .catch((err) => {
-            throw err;
-        });
-}
-
-const corsOptions = {
-    // AccessControlAllowOrigin: '*',
-    origin: "https://dashing-bombolone-1ea557.netlify.app/",
-   //  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    Credential: true,
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("Connected to DB");
+    })
+    .catch((err) => {
+      throw err;
+    });
 };
 
-app.use(cors(corsOptions));
+// const _dirname= path.resolve();
+
+//middlewares
 app.use(cookieParser())
-app.use(express.json())
-app.use("/api/auth", authRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/videos", videoRoutes)
-app.use("/api/comments", commentRoutes)
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
 
+// app.use(express.static(path.join(_dirname, "/youtube-clone/build")))
+// app.get('*', (_,res)=>{
+//   res.sendFile(path.resolve(_dirname, "youtube-clone", "build", "index.html"));
+// });
+//error handler
 app.use((err, req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    
-    const status = err.status || 500;
-    const message = err.status || "something went wrong!";
-    return res.status(status).json({
-        success: false,
-        status,
-        message,
-    })
-})
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong!";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
 
-app.listen(8800, () => {
-    connect()
-    console.log("connected to server!!")
-})
+app.listen(port, () => {
+  connect();
+  console.log("Connected to Server");
+});
